553c3361806eaf60aed7c959293304284e560363
Kunal Prakash <7422639+kp297@users.noreply.github.com>
2022-12-07 23:00:01 -0500

    Add files via upload

---
+++ labs/scrabble/scrabble.c
@@ -0,0 +1,62 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Points assigned to each letter of the alphabet
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+int compute_score(string word);
+
+int main(void)
+{
+    // Get input words from both players
+    string word1 = get_string("Player 1: ");
+    string word2 = get_string("Player 2: ");
+
+    // Score both words
+    int score1 = compute_score(word1);
+    int score2 = compute_score(word2);
+
+    printf("Score 1: %d\n", score1);
+    printf("Score 2: %d\n", score2);
+
+    // TODO: Print the winner
+    if(score1 > score2)
+    {
+        printf("Player 1 wins!\n");
+    }
+    else if (score1 < score2)
+    {
+        printf("Player 2 wins!\n");
+    }
+    else
+    {
+        printf("Tie!\n");
+    }
+}
+
+int compute_score(string word)
+{
+    int l = strlen(word);
+    string alph = "abcdefghijklmnopqrstuvwxyz";
+    int count = 0;
+
+    for (int i = 0; i < l; i++)
+    {
+        for (int j = 0; j < 26; j++)
+        {
+            if (tolower(word[i]) == alph[j])
+            {
+                count = count + POINTS[j];
+                break;
+            }
+        }
+
+
+    }
+    return count;
+
+
+    // TODO: Compute and return score for string
+}
